159efe02f2d693dc82f21e50c9b820045621719b
Bkumar48 <bittukumarinfo48@gmail.com>
2024-07-16 09:00:29 +0530

    [UPDATE] -images added

--- src/app/(pages)/services/[slug]/page.tsx
+++ src/app/(pages)/services/[slug]/page.tsx
@@ -45,6 +45,18 @@ export async function generateMetadata({
   };
 }
 
+export async function generateStaticParams() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URI}/service/getServices`
+  );
+  const data = await res.json();
+  const newData = data;
+  return newData.map((service: any) => ({
+    slug: service.slug.toString(),
+  }));
+}
+
+
 interface ServiceProps {
   params: {
     slug: string;
